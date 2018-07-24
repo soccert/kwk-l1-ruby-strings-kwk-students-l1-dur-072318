1159bc24e5bee3dacd65f478f5dabe0c0647327e
soccert <talyagriffin2004@gmail.com>
2018-07-24 13:57:01 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,8 +1,3 @@
 # Code your prompts here!
 
-# Try starting out with puts'ing a string.
-puts "Hermione"
-puts "Best Halloween party ever"
-puts "October 31"
-puts "6pm"
-puts "Harry Potter"
+# Try starting out with puts'ing a string
